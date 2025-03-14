b8df735cdd83a45a1a6b3eea5e6b583e8034ab83
Геннадий Жиляев <zhilyaevge@yandex.ru>
2025-03-14 22:07:36 +0300

    first commit

---
+++ main_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMainHandlerWhenMissingCount(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	status := responseRecorder.Code
+	require.Equal(t, http.StatusBadRequest, status)
+
+	body := responseRecorder.Body.String()
+	require.NotEmpty(t, body)
+}
+
+func TestMainHandlerWhenWrongCity(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=4&city=tula", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	status := responseRecorder.Code
+	require.Equal(t, http.StatusBadRequest, status)
+
+	city := req.URL.Query().Get("city")
+	require.NotEqual(t, city, "moscow")
+}
+
+func TestMainHandlerWhenWrongCount(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=moscow&city=tula", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	status := responseRecorder.Code
+	require.Equal(t, http.StatusBadRequest, status)
+
+	countStr := req.URL.Query().Get("count")
+	_, err := strconv.Atoi(countStr)
+	require.Error(t, err)
+}
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
+	assert.Len(t, list, totalCount)
+}
+
+func TestMainHandlerWhenOk(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	status := responseRecorder.Code
+	require.Equal(t, http.StatusOK, status)
+}
